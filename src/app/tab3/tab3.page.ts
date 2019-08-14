c0e46d5561ca4ae0d42559d0e7d548ec6a1e5ece
unknown <g_sujeeharan@yahoo.com>
2019-08-14 16:10:12 +0530

    Map added

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { GoogleMaps, GoogleMap, Environment } from '@ionic-native/google-maps/ngx';
+import { GoogleMaps, GoogleMap, Environment, Circle } from '@ionic-native/google-maps/ngx';
 import { fromEventPattern } from 'rxjs';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { ILatLng, Marker } from '@ionic-native/google-maps/ngx';
 
- 
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -18,14 +19,46 @@ export class Tab3Page implements OnInit {
     await this.platform.ready;
     await this.loadMap();
   }
+  
+  
 
-  constructor(private platform:Platform) {}
+  constructor(private platform: Platform) { }
 
-  loadMap(){
+  loadMap() {
     Environment.setEnv({
-      'API_KEY_FOR_RELEASE':'AIzaSyAuwf7K_hn-vY0Ea2RtiBR2OEV1o0ousbw'
+      'API_KEY_FOR_RELEASE': 'AIzaSyAuwf7K_hn-vY0Ea2RtiBR2OEV1o0ousbw'
     });
 
     this.map = GoogleMaps.create('map_canvas');
+    
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'You are here',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 6.9,
+        lng: 79
+      }
+    });
+    let circle : Circle = this.map.addCircleSync({
+      'center':this.center,
+      'radius': this.radius,
+      'strokeColor': '#AA00FF',
+      'strokeWidth':5,
+       'fillColor': '#00880055'
+     });
   }
+
+  center: ILatLng = { "lat": 6.9, "lng": 79 };
+
+  radius = 300;
+
+  
+
+ 
+
+
+
+
+
 }
