a238254a9172ab4d06bd5cceb7efc9dc8cf217c5
unknown <g_sujeeharan@yahoo.com>
2019-09-08 12:57:25 +0530

    url edited

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -13,13 +13,21 @@ export class QuestionsService {
       
     });
    }
-
+// Uvindu's Back end
    public getJSON(): Observable<any> {
      return this.http.get("http://127.0.0.1:5000/questions/");
    }
 
+   //Submit answers to Backend
    public postAnswers(answer): Observable<any> {
      return this.http.post("http://127.0.0.1:5000/submit/", {ans : answer})
+   }
+
+   //Personal Disorder Questions
+   public getPersonalQuestions(): Observable<any> {
+     return this.http.post("http://127.0.0.1.:5000/Personalquestions", {
+       ans: 
+     })
    }
 
 }
